c7fb7f2313c4c35370e0e316d2d344e7e05b2fe7
Ksenia Brodulenko <151148388+kseniabro@users.noreply.github.com>
2024-02-21 12:14:16 +0300

    Delete NewsPaper directory

--- NewsPaper/app_news/permissions.py
+++
@@ -1,1 +0,0 @@
-PUBLISH_NEWS_PERM_CODE_NAME = 'can_publish_news'
--- NewsPaper/app_news/static/app_news/js/change_localization.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Переключение языка сайта.
-    let select_obj = document.querySelector('select[name="language"]'),
-        change_lang_form = document.querySelector('form[name="setLang"]');
-
-    select_obj.addEventListener('change', function () {
-        change_lang_form.submit()
-    })
-});
