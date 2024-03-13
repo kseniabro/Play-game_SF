7e4d12e34f19b92ed24a8d170518c4cb98dc0dc0
Ksenia Brodulenko <151148388+kseniabro@users.noreply.github.com>
2024-03-13 16:15:27 +0300

    Update runapscheduler.py

--- NewsPaper/news/management/commands/runapscheduler.py
+++ NewsPaper/news/management/commands/runapscheduler.py
@@ -17,9 +17,7 @@ from NewsPaper.NewsPaper import settings
 logger = logging.getLogger(__name__)
 
 
-# наша задача по выводу текста на экран
 def my_job():
-    #  Your job processing logic here...
     today = datetime.datetime.now()
     last_week = today - datetime.timedelta(days=7)
     posts = Post.objects.filter(creation_date__gte=last_week)
@@ -40,7 +38,7 @@ def my_job():
     )
 
 
-# функция, которая будет удалять неактуальные задачи
+
 def delete_old_job_executions(max_age=604_800):
     """This job deletes all apscheduler job executions older than `max_age` from the database."""
     DjangoJobExecution.objects.delete_old_job_executions(max_age)
@@ -53,14 +51,13 @@ class Command(BaseCommand):
         scheduler = BlockingScheduler(timezone=settings.TIME_ZONE)
         scheduler.add_jobstore(DjangoJobStore(), "default")
 
-        # добавляем работу нашему задачнику
+       
         scheduler.add_job(
             my_job,
             trigger=CronTrigger(
-                day_of_week="sat", hour="10", minute="00"
+                day_of_week="fri", hour="18", minute="00"
             ),
-            # То же, что и интервал, но задача тригера таким образом более понятна django
-            id="my_job",  # уникальный айди
+            id="my_job",  
             max_instances=1,
             replace_existing=True,
         )
@@ -71,8 +68,6 @@ class Command(BaseCommand):
             trigger=CronTrigger(
                 day_of_week="mon", hour="00", minute="00"
             ),
-            # Каждую неделю будут удаляться старые задачи, которые либо не удалось выполнить,
-            # либо уже выполнять не надо.
             id="delete_old_job_executions",
             max_instances=1,
             replace_existing=True,
