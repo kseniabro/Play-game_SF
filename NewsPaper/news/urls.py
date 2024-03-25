f542edd0b73f18c354ddbac1f424604f538716ac
Ksenia Brodulenko <151148388+kseniabro@users.noreply.github.com>
2024-03-25 10:28:53 +0300

    Update urls.py

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -6,6 +6,7 @@ from django.views.decorators.cache import cache_page
 urlpatterns = [
     path('', PostsList.as_view(), name='news'),
     path('search/', PostSearch.as_view(), name='search'),
+    path('news/<int:pk>/', cache_page(60*5)(PostDetailView.as_view()), name='some_news'),
     path('add/', PostCreate.as_view(), name='post_create'),
     path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
     path('<int:pk>/edit/', PostUpdate.as_view(), name='post_update'),
